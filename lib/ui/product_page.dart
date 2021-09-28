e439f783e07593d647da3f05e1eddaf7ff00407e
K-shir0 <kakishiro2190394@gmail.com>
2021-09-29 05:12:03 +0900

    change: 商品詳細ページに遷移するようにした.

--- lib/ui/product_page.dart
+++ lib/ui/product_page.dart
@@ -1,15 +1,21 @@
+import 'package:auto_route/annotations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// 商品を個別に表示する画面.
 class ProductPage extends StatelessWidget {
-  const ProductPage({Key? key}) : super(key: key);
+  const ProductPage({
+    Key? key,
+    @PathParam('id') required this.id,
+  }) : super(key: key);
+
+  final String id;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: Text('id: $id'),
     );
   }
 }
--- lib/ui/route/app_route.dart
+++ lib/ui/route/app_route.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 
 import '../home_page.dart';
+import '../product_page.dart';
 
 export 'app_route.gr.dart';
 
@@ -11,6 +12,10 @@ export 'app_route.gr.dart';
       path: '/',
       page: HomePage,
       initial: true,
+    ),
+    AutoRoute(
+      path: '/products/:id',
+      page: ProductPage,
     ),
   ],
 )
